6a108edfbd010cac24cb5f3df77cca5397b34338
juancr83 <juancr83@outlook.com>
2023-02-17 22:26:12 -0600

    [REF] Agregando cambios de diarios.

--- models/models.py
+++ models/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class agro(models.Model):
-#     _name = 'agro.agro'
-#     _description = 'agro.agro'
+class agro(models.Model):
+    _name = 'agro.agro'
+    _description = 'agro.agro'
 
 #     name = fields.Char()
 #     value = fields.Integer()
@@ -16,3 +16,28 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+    
+    def create_invoices(self):
+        rst = super(SaleAdvancePaymentInv, self).create_invoices()
+        print("Hola mundo")
+        return rst
+    
+    def _create_invoice(self, order, so_line, amount):
+        print("--------------------------------------------------------")
+        invoice = super(SaleAdvancePaymentInv, self)._create_invoice()
+        print(invoice)
+        return invoice
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    
+    def _create_invoices(self, grouped=False, final=False, date=None):
+        invoice = super(SaleOrder, self)._create_invoices()
+        print(invoice)
+        for line in invoice.invoice_line_ids:
+            if line.product_id.categ_id == self.env.ref("agro.producto_categoria_1"):
+                invoice.journal_id = self.env.ref('agro.sale_fa_ve_al_nu_co')
+        return invoice
