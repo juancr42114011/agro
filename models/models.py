96629803bbcea88f48bc8239c065586f90381189
juancr83 <juancr83@outlook.com>
2023-02-20 22:27:02 -0600

    [REF] Agregando validacion por categoria de productos alimentos nutricionales.

--- models/models.py
+++ models/models.py
@@ -1,43 +1,51 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
 
 
-class agro(models.Model):
-    _name = 'agro.agro'
-    _description = 'agro.agro'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
-class SaleAdvancePaymentInv(models.TransientModel):
-    _inherit = "sale.advance.payment.inv"
+class ProductCategory(models.Model):
+    _inherit = "product.category"
     
-    def create_invoices(self):
-        rst = super(SaleAdvancePaymentInv, self).create_invoices()
-        print("Hola mundo")
-        return rst
-    
-    def _create_invoice(self, order, so_line, amount):
-        print("--------------------------------------------------------")
-        invoice = super(SaleAdvancePaymentInv, self)._create_invoice()
-        print(invoice)
-        return invoice
+    #Funcion para buscar si el producto pertenece a una categoria en especifica.
+    def verificarCategoria(self, categ):
 
+        res = self.get_external_id()
+        if res.get(self.id) == categ:
+            return True
+        
+        if self.parent_id:
+            return self.parent_id.verificarCategoria(categ)
+        
+        return False
+        
 class SaleOrder(models.Model):
     _inherit = "sale.order"
     
     def _create_invoices(self, grouped=False, final=False, date=None):
         invoice = super(SaleOrder, self)._create_invoices()
-        print(invoice)
         for line in invoice.invoice_line_ids:
-            if line.product_id.categ_id == self.env.ref("agro.producto_categoria_1"):
-                invoice.journal_id = self.env.ref('agro.sale_fa_ve_al_nu_co')
+            if line.product_id.categ_id.verificarCategoria('agro.producto_categoria_1'):
+                invoice.journal_id = self.env.ref('agro.sale_fa_ve_al_nu_co').id
         return invoice
+
+    def _action_confirm(self):
+        #Vamos a validar que todos los productos pertenezcan a la misma categoria o categoria padre
+        validando_alimentos = {}
+        for line in self.order_line:
+            categoria = line.product_id.categ_id
+            validando_alimentos[line.id] = categoria.verificarCategoria('agro.producto_categoria_1')
+        
+        contador_verdadero = 0
+        contador_falso = 0
+        for key in validando_alimentos.keys():
+            if validando_alimentos[key]:
+                contador_verdadero += 1
+            else:
+                contador_falso += 1
+        
+        if contador_verdadero > 0 and contador_falso > 0:
+            raise UserError(_('No se pueden mezclar productos de lineas diferentes de categoria.'))  
+               
+        return super(SaleOrder, self)._action_confirm()
+        
