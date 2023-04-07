40ed84497a94999deccf68d34a8c9973ef2ebc22
juancr83 <juancr83@outlook.com>
2023-04-07 12:50:47 -0600

    [REF] Creando modelo de presupuesto.

--- __manifest__.py
+++ __manifest__.py
@@ -18,20 +18,22 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.3',
+    'version': '0.6',
 
     # any module necessary for this one to work correctly
     'depends': ['imporgesa','stock'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/product_template_views.xml',
         'data/account_account_data.xml',
         'data/product_template_data.xml',
         'data/account_journal_data.xml',
+        'data/ir_sequence_data.xml',
+        'views/presupuesto_venta_views.xml',
         'report/sale_report.xml',
     ],
     # only loaded in demonstration mode
--- models/presupuesto_venta.py
+++ models/presupuesto_venta.py
@@ -5,11 +5,51 @@ from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessE
 
 class PresupuestoVenta(models.Model):
     _name = "presupuesto.venta"
-    _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin', 'sequence.mixin']
+    _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin']
     _description = "Presupuesto de Venta"
     _order = 'id desc'
     _mail_post_access = 'read'
     _check_company_auto = True
     
-    name = fields.Char(string='Number', copy=False, compute='_compute_name', readonly=False, store=True, index=True, tracking=True)
-  
+    name = fields.Char(string='Number', 
+                       copy=False, 
+                       readonly=True, 
+                       store=True, 
+                       index=True,
+                       )
+    date = fields.Date(
+        string='Date',
+        required=True,
+        index=True,
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        copy=False,
+        tracking=True,
+        default=fields.Date.context_today
+    )
+    state = fields.Selection(selection=[
+            ('draft', 'Draft'),
+            ('posted', 'Posted'),
+            ('cancel', 'Cancelled'),
+        ], string='Status', required=True, readonly=True, copy=False, tracking=True,
+        default='draft')
+    company_id = fields.Many2one(comodel_name='res.company', string='Company',
+                                 store=True, readonly=True,
+                                 compute='_compute_company_id')
+    
+    @api.model
+    def create(self, vals):
+        company_id = vals.get('company_id', self.default_get(['company_id'])['company_id'])
+        self_comp = self.with_company(company_id)
+        if vals.get('name', 'New') == 'New':
+            seq_date = None
+            if 'date' in vals:
+                seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(vals['date']))
+            vals['name'] = self_comp.env['ir.sequence'].next_by_code('presupuesto.venta', sequence_date=seq_date) or '/'
+        
+        res = super(PresupuestoVenta, self_comp).create(vals)
+        return res
+
+    def _compute_company_id(self):
+        for move in self:
+            move.company_id = move.journal_id.company_id or move.company_id or self.env.company
