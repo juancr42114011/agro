a2fb691f75b23da86fce8c203051f0f31eead66c
juancr83 <juancr83@outlook.com>
2023-04-07 08:00:34 -0600

    [REF] Agregando cuentas analiticas en la categoria.

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import product_label_layout
 from . import product_template
+from . import account_move
--- models/product_template.py
+++ models/product_template.py
@@ -6,3 +6,10 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
     
     registro_sanitario = fields.Char(string="Registro Sanitario")
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+    
+    analytic_account_id = fields.Many2one('account.analytic.account', string='Cuenta Analitica',
+        index=True, store=True, readonly=False, company_dependent=True, copy=True)
+    
