02fb64ba3eb2e857233eff299486f0e12d694e4c
juancr83 <juancr83@outlook.com>
2023-05-14 08:59:29 -0600

    [REF] Quitando presupeusto de venta parte 2

--- models/presupuesto_venta.py
+++ models/presupuesto_venta.py
@@ -12,31 +12,3 @@ import base64
 import io
 import logging
 
-class PresupuestoVenta(models.Model):
-    _name = "presupuesto.venta"
-    _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin']
-    _description = "Presupuesto de Venta"
-    _order = 'id desc'
-    _mail_post_access = 'read'
-    _check_company_auto = True
-    
-    READONLY_STATES = {
-        'purchase': [('readonly', True)],
-        'done': [('readonly', True)],
-        'cancel': [('readonly', True)],
-    }
-    
-    name = fields.Char(string='Number', 
-                       copy=False, 
-                       readonly=True, 
-                       store=True, 
-                       index=True,
-                       )
-
-
-class PresupuestoVentaLine(models.Model):
-    _name = 'presupuesto.venta.line'
-    _description = 'Presupuesto Venta Line'
-    _order = 'order_id, sequence, id'
-
-    name = fields.Text(string='Description', required=True)
